3c17b7583df6b22ae882a6fff0852a8834c398ab
Mike <slavo5150@yahoo.com>
2015-01-16 12:47:18 +0900

    Add hack to deal with GCC bug 192

--- build.d
+++ build.d
@@ -11,17 +11,32 @@ void main(string[] args)
 {
     auto sourceDir = "source";
     auto binaryDir = "binary";
+    auto assemblyFile1 = buildPath(binaryDir, "firmware_temp.s");
+    auto assemblyFile2 = buildPath(binaryDir, "firmware.s");
+    auto objectFile = buildPath(binaryDir, "firmware.o");
     auto outputFile = buildPath(binaryDir, "firmware");
     
     string cmd = "rm -f " ~ outputFile;
     system(cmd);
 
-    cmd = "arm-none-eabi-gdc -O3 -nophoboslib -nostdinc -nodefaultlibs -nostdlib -fno-emit-moduleinfo -ffunction-sections -fdata-sections"
-          ~ " "
+    cmd = "arm-none-eabi-gdc -c -O3 -nophoboslib -nostdinc -nodefaultlibs -nostdlib -fno-emit-moduleinfo -ffunction-sections -fdata-sections"
+          ~ " -S"
           ~ " " ~ sourceDir.dirEntries("*.d", SpanMode.depth).map!"a.name".join(" ")
-          ~ " -Wl,-Tsource/linker/linker.ld -Wl,--gc-sections"
-          ~ " -o " ~ outputFile;                  
+          //~ " -Wl,-Tsource/linker/linker.ld -Wl,--gc-sections"
+          ~ " -o " ~ assemblyFile1;                  
             
+    writeln(cmd);
+    system(cmd);
+    
+    cmd = `sed -e 's/^\(\.LC[0-9]*\)\(\:\)/\.section .rodata\1\n\1\2/g' ` ~ assemblyFile1 ~ " >" ~ assemblyFile2;
+    writeln(cmd);
+    system(cmd);
+    
+    cmd = "arm-none-eabi-as " ~ assemblyFile2 ~ " -o " ~ objectFile;
+    writeln(cmd);
+    system(cmd);
+    
+    cmd = "arm-none-eabi-ld " ~ objectFile ~ " -Tsource/linker/linker.ld --gc-sections -o " ~ outputFile;
     writeln(cmd);
     system(cmd);
     
