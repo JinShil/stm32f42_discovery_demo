25ce259a36f8f74467dea56aab4b66ef1b121750
JinShil <slavo5150@yahoo.com>
2017-07-19 20:11:02 +0900

    More automation to GDB script

--- gdb.sh
+++ gdb.sh
@@ -1,2 +1,1 @@
-
-arm-none-eabi-gdb binary/firmware -ex "target remote localhost:3333" -ex "monitor arm semihosting enable"
+arm-none-eabi-gdb binary/firmware -ex "target remote localhost:3333" -ex "monitor arm semihosting enable" -ex "monitor reset halt" -ex "load" -ex "monitor reset init" -ex "continue"
